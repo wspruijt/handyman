6a36fa36a5d1862443a72fb395a76379ce84827e
Willem Spruijt <willem.spruijt@gmail.com>
2012-10-16 08:53:17 +0200

    Added commands

--- handyman.sh
+++ handyman.sh
@@ -5,6 +5,18 @@
 # Make sure you have generated a key using ssh-keygen -t rsa
 cat ~/.ssh/id_rsa.pub | ssh user@hostname 'cat >> .ssh/authorized_keys'
 
+# --- Fix UTF8 encoding ---
 # Fix the annoying 'Convert to UTF-8'-notice while intalling gems
 # You can add this to your .bashrc or .zshrc
 export LC_CTYPE="UTF-8" 
+
+# --- Recursively delete .svn directories ---
+# Recursively delete all .svn directories under a specific directory
+find . -name .svn -exec 'rm -rf {}\;'
+
+# --- Useful ls  ----
+# List (l) all (a) files and directories, order by modification date (t), reversed (r)
+# Mnemonic: ls -lat(e)r
+ls -latr
+alias ls='ls -latr' # Add to your .bashrc/.zshrc to make the options default
+
